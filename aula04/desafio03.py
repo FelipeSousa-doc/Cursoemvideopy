90922ad70aada8c33b0b713f51e4edc85346b6b0
FelipeSousa-doc <felipesousaalvesfs@gmail.com>
2024-11-15 22:53:37 -0300

    aula 4

---
+++ aula04/desafio03.py
@@ -0,0 +1,11 @@
+"""
+Crie um script Python que leia dois números e tente
+mostrar a soma entre eles.
+"""
+print('-' * 40)
+print('          DESAFIO 03      ')
+primeiro_numero = int(input('Primeiro número: '))
+segundo_numero = int(input('Segundo número: '))
+soma = primeiro_numero + segundo_numero
+
+print('A soma entre ',primeiro_numero, 'e', segundo_numero, 'é', soma)
