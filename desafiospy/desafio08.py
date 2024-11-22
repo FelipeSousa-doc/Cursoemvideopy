d007f3198f0e9c507314c28b39bab0372077ae27
FelipeSousa-doc <felipesousaalvesfs@gmail.com>
2024-11-21 22:11:27 -0300

    novos codigos

---
+++ desafiospy/desafio08.py
@@ -0,0 +1,10 @@
+"""
+ Faça um programa que leia um número Inteiro
+ e mostre na tela o seu sucessor e seu antecessor.
+"""
+numero = int(input('Digite um número: '))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f'Analisando o valor {numero}, o antecessor é {antecessor} e o sucessor é {sucessor}')
